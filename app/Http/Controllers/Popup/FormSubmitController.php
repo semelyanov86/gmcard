979f188e0c9ee42519964b5f8035124d4949744b
Kirillmlk <kostakov.kiirill@yandex.by>
2025-07-25 13:26:53 +0300

    feat: add PopupFormAction

--- app/Http/Controllers/Popup/FormSubmitController.php
+++ app/Http/Controllers/Popup/FormSubmitController.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Popup;
 
+use App\Actions\Popup\PopupFormAction;
 use App\Data\PopUpData;
 use App\Http\Requests\Popup\PopUpFormRequest;
 use App\Http\Controllers\Controller;
-use App\Services\Popup\PopupFormService;
 use Illuminate\Http\RedirectResponse;
 
 class FormSubmitController extends Controller
 {
-    public function __construct(protected PopupFormService $service) {}
+    public function __construct(protected PopupFormAction $service) {}
 
     public function submit(PopUpFormRequest $request): RedirectResponse
     {
         $dto = PopUpData::from($request);
-        $crmResponse = $this->service->handle($dto);
+        $this->service->handle($dto);
 
         return redirect()->back()->with('success', 'Заявка отправлена!');
     }
