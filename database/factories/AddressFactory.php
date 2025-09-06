b8c25f0cf94caf3e0691dbfff5356e3652c67912
Kirillmlk <kostakov.kiirill@yandex.by>
2025-09-06 17:54:46 +0300

    feat: add AddressFactory

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name'            => $this->faker->company(),
+            'open_hours'      => '9:00-18:00',
+            'phone'           => $this->faker->phoneNumber(),
+            'phone_secondary' => $this->faker->phoneNumber(),
+            'website'         => $this->faker->url(),
+        ];
+    }
+}
