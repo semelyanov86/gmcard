6673f790be08cd760aaf0712bdbacb89b533511b
Kirillmlk <kostakov.kiirill@yandex.by>
2025-09-06 18:03:41 +0300

    feat: add CategoryFactory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    public function definition(): array
+    {
+
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'parent_id' => null,
+            'is_starred' => $this->faker->boolean(),
+        ];
+    }
+}
