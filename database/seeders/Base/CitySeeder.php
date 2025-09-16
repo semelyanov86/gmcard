f46342d41a6bd8f1ec203fe9f15b3203d96fa170
Kirillmlk <kostakov.kiirill@yandex.by>
2025-09-16 11:21:21 +0300

    fix: add length limit to country in cities

--- database/seeders/Base/CitySeeder.php
+++ database/seeders/Base/CitySeeder.php
@@ -12,16 +12,16 @@ class CitySeeder extends Seeder
     public function run(): void
     {
         $cities = [
-            ['name' => 'Москва', 'country' => 'Россия'],
-            ['name' => 'Санкт-Петербург', 'country' => 'Россия'],
-            ['name' => 'Новосибирск', 'country' => 'Россия'],
-            ['name' => 'Екатеринбург', 'country' => 'Россия'],
-            ['name' => 'Казань', 'country' => 'Россия'],
-            ['name' => 'Нижний Новгород', 'country' => 'Россия'],
-            ['name' => 'Челябинск', 'country' => 'Россия'],
-            ['name' => 'Самара', 'country' => 'Россия'],
-            ['name' => 'Уфа', 'country' => 'Россия'],
-            ['name' => 'Ростов-на-Дону', 'country' => 'Россия'],
+            ['name' => 'Москва', 'country' => 'ru'],
+            ['name' => 'Санкт-Петербург', 'country' => 'ru'],
+            ['name' => 'Новосибирск', 'country' => 'ru'],
+            ['name' => 'Екатеринбург', 'country' => 'ru'],
+            ['name' => 'Казань', 'country' => 'ru'],
+            ['name' => 'Минск', 'country' => 'by'],
+            ['name' => 'Алматы', 'country' => 'kz'],
+            ['name' => 'Астана', 'country' => 'kz'],
+            ['name' => 'Киев', 'country' => 'ua'],
+            ['name' => 'Ташкент', 'country' => 'uz'],
         ];
 
         foreach ($cities as $data) {
