b6fc66af6536e0cbbe2193cc6ea1b2014cbed4d0
Kirillmlk <kostakov.kiirill@yandex.by>
2025-10-01 15:06:49 +0300

    feat: add icons and password visibility toggle, fix error display

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
 import InputError from '@/components/InputError.vue';
-import TextLink from '@/components/TextLink.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import AuthBase from '@/layouts/AuthLayout.vue';
-import { Head, useForm } from '@inertiajs/vue3';
-import { LoaderCircle } from 'lucide-vue-next';
+import { useForm } from '@inertiajs/vue3';
 import AdaptiveImage from '@/components/ui/AdaptiveImage.vue';
+import { ref } from 'vue';
 
 const form = useForm({
     name: '',
@@ -20,6 +15,9 @@ const form = useForm({
     code: '',
 });
 
+const showPassword = ref(false);
+const showPasswordConfirmation = ref(false);
+
 const submit = () => {
     form.post(route('register'), {
         onFinish: () => form.reset('password', 'password_confirmation'),
@@ -31,15 +29,15 @@ const submit = () => {
 
     <section class="w-full h-full flex items-center justify-center pt-10 pb-14">
         <div class="w-[500px] flex items-center flex-col justify-center bestForm">
-            <AdaptiveImage image-path="gm-logo" alt="logo" class="w-14 h-14" :critical="true"></AdaptiveImage>
+            <AdaptiveImage image-path="logo" alt="logo" class="w-14 h-14" :critical="true"></AdaptiveImage>
             <h1 class="text-[32px] font-bold w-[550px] text-center lett mt-5 tracking-tighter leading-tight text-black bestForm">Зарегистрируйтесь и подключитесь к системе</h1>
             <div class="flex flex-col items-center mt-5">
                 <h4 class="text-lg font-bold">Зарегистрироваться через</h4>
                 <ul class="flex items-center mt-5 gap-8">
-                    <li><a href=""><img src="/assets/cosial/google.svg" alt="Через google" class="w-10 h-10 hover:opacity-70"></a></li>
-                    <li><a href=""><img src="/assets/cosial/facebook.svg" alt="Через facebook" class="w-10 h-10 hover:opacity-70"></a></li>
-                    <li><a href=""><img src="/assets/cosial/ok.svg" alt="Через ok" class="w-10 h-10 hover:opacity-70"></a></li>
-                    <li><a href=""><img src="/assets/cosial/vk.svg" alt="Через vk" class="w-10 h-10 hover:opacity-70"></a></li>
+                    <li><a href=""><img src="/images/svg/google.svg" alt="Через google" class="w-10 h-10 hover:opacity-70"></a></li>
+                    <li><a href=""><img src="/images/svg/facebook.svg" alt="Через facebook" class="w-10 h-10 hover:opacity-70"></a></li>
+                    <li><a href=""><img src="/images/svg/ok.svg" alt="Через ok" class="w-10 h-10 hover:opacity-70"></a></li>
+                    <li><a href=""><img src="/images/svg/vk_r.svg" alt="Через vk" class="w-10 h-10 hover:opacity-70"></a></li>
                 </ul>
             </div>
             <div class="flex items-center my-6">
@@ -51,7 +49,7 @@ const submit = () => {
                 <h2 class="text-lg font-bold">Укажите электронную почту и пароль</h2>
                 <div class="flex flex-col w-full mt-5 relative">
                     <label for="name" class="text-sm font-semibold mb-2">Ваше имя</label>
-                    <input id="name" type="text" equired autofocus :tabindex="1" autocomplete="name" v-model="form.name" placeholder="Ваше имя" name="name" class="border border-black/40 p-3 w-full rounded-md">
+                    <input id="name" type="text" required autofocus :tabindex="1" autocomplete="name" v-model="form.name" placeholder="Ваше имя" name="name" class="border border-black/40 p-3 w-full rounded-md">
                     <i class="text-[12px] opacity-50 absolute right-1 top-0">* обязательное поле</i>
                     <InputError :message="form.errors.name" />
                 </div>
@@ -67,6 +65,12 @@ const submit = () => {
                         v-model="form.birth_date"
                         class="border border-black/40 p-3 w-full rounded-md"
                     >
+                    <p v-if="form.errors.birth_date" class="flex items-center gap-1 text-xs text-red-500 mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        </svg>
+                        {{ form.errors.birth_date }}
+                    </p>
                 </div>
                 <div class="w-full mt-5 relative">
                     <p class="text-sm font-semibold mb-2">Укажите свой пол</p>
@@ -102,63 +106,79 @@ const submit = () => {
                             <label for="gender3" class="text-base font-medium cursor-pointer">Другой вариант</label>
                         </div>
                     </div>
+                    <p v-if="form.errors.gender" class="flex items-center gap-1 text-xs text-red-500 mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        </svg>
+                        {{ form.errors.gender }}
+                    </p>
                 </div>
 
                 <div class="flex flex-col w-full mt-5 relative">
                     <label for="email" class="text-sm font-semibold mb-2">Ваш адрес электронной почты</label>
                     <input id="email" type="email" placeholder="Ваш адрес электронной почты" name="email" required :tabindex="2" autocomplete="email" v-model="form.email" class="border border-black/40 p-3 w-full rounded-md">
                     <i class="text-[12px] opacity-50 absolute right-1 top-0">* обязательное поле</i>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
+                    <p v-if="form.errors.email" class="flex items-center gap-1 text-xs text-red-500 mt-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                         </svg>
-                        Данный email уже зарегистрирован в системе, нажмите на кнопку "Войти"</p>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                        Введите данные корректно. Пример: "example@gmail.com"</p>
+                        {{ form.errors.email }}
+                    </p>
                 </div>
                 <div class="flex flex-col w-full mt-5 relative">
                     <label for="password" class="text-sm font-semibold mb-2">Придумайте пароль</label>
-                    <input id="password" type="password" minlength="8" placeholder="Придумай пароль" name="password" required :tabindex="3" autocomplete="new-password" v-model="form.password" class="border border-black/40 p-3 pr-10 w-full rounded-md">
-                    <img src="/assets/cosial/passSee.svg" alt="Показать пароль" id="show_password" class="w-5 h-5 absolute right-4 top-10 opacity-70 hover:opacity-100 cursor-pointer">
+                    <input
+                        id="password"
+                        :type="showPassword ? 'text' : 'password'"
+                        minlength="8"
+                        placeholder="Придумай пароль"
+                        name="password"
+                        required
+                        :tabindex="3"
+                        autocomplete="new-password"
+                        v-model="form.password"
+                        class="border border-black/40 p-3 pr-10 w-full rounded-md"
+                    >
+                    <img
+                        src="/images/svg/passSee.svg"
+                        alt="Показать пароль"
+                        @click="showPassword = !showPassword"
+                        class="w-5 h-5 absolute right-4 top-10 opacity-70 hover:opacity-100 cursor-pointer"
+                    >
                     <i class="text-[12px] opacity-50 absolute right-1 top-0">* обязательное поле</i>
-                    <div class="w-full bg-green-400 h-2 my-2 hidden" id="success"></div>
-                    <div class="w-[300px] bg-yellow-400 h-2 my-2 hidden" id="med_success"></div>
-                    <div class="w-[150px] bg-red-400 h-2 my-2 hidden" id="failed"></div>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
+                    <p v-if="form.errors.password" class="flex items-center gap-1 text-xs text-red-500 mt-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                         </svg>
-                        Пароль должен содержать латинские символы, цифры, @, _, %, - и заглавную букву</p>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                        Длинна пароля должна быть не меньше 8 и не больше 16 символов
+                        {{ form.errors.password }}
                     </p>
                 </div>
 
                 <div class="flex flex-col w-full mt-5 relative">
                     <label for="password_confirmation" class="text-sm font-semibold mb-2">Подтвердите пароль</label>
                     <input
-                        id="password_confirmation" type="password" required :tabindex="4" autocomplete="new-password" v-model="form.password_confirmation" minlength="8" placeholder="Подтвердите  пароль" name="password" class="border border-black/40 p-3 pr-10 w-full rounded-md">
-                    <img src="/assets/cosial/passSee.svg" alt="Показать пароль" id="show_password" class="w-5 h-5 absolute right-4 top-10 opacity-70 hover:opacity-100 cursor-pointer">
+                        id="password_confirmation"
+                        :type="showPasswordConfirmation ? 'text' : 'password'"
+                        required
+                        :tabindex="4"
+                        autocomplete="new-password"
+                        v-model="form.password_confirmation"
+                        minlength="8"
+                        placeholder="Подтвердите пароль"
+                        class="border border-black/40 p-3 pr-10 w-full rounded-md"
+                    >
+                    <img
+                        src="/images/svg/passSee.svg"
+                        alt="Показать пароль"
+                        @click="showPasswordConfirmation = !showPasswordConfirmation"
+                        class="w-5 h-5 absolute right-4 top-10 opacity-70 hover:opacity-100 cursor-pointer"
+                    >
                     <i class="text-[12px] opacity-50 absolute right-1 top-0">* обязательное поле</i>
-                    <div class="w-full bg-green-400 h-2 my-2 hidden" id="success"></div>
-                    <div class="w-[300px] bg-yellow-400 h-2 my-2 hidden" id="med_success"></div>
-                    <div class="w-[150px] bg-red-400 h-2 my-2 hidden" id="failed"></div>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
+                    <p v-if="form.errors.password_confirmation" class="flex items-center gap-1 text-xs text-red-500 mt-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                         </svg>
-                        Пароль должен содержать латинские символы, цифры, @, _, %, - и заглавную букву</p>
-                    <p class="flex items-center gap-1 text-xs text-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                        Длинна пароля должна быть не меньше 8 и не больше 16 символов
+                        {{ form.errors.password_confirmation }}
                     </p>
                 </div>
                 <div class="flex flex-col w-full mt-5 relative">
@@ -170,9 +190,14 @@ const submit = () => {
                         placeholder="Код (можно буквенный) пригласившего вас человека"
                         class="border border-black/40 p-3 w-full rounded-md"
                     >
+                    <p v-if="form.errors.code" class="flex items-center gap-1 text-xs text-red-500 mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        </svg>
+                        {{ form.errors.code }}
+                    </p>
                 </div>
                 <button type="submit" tabindex="5" :disabled="form.processing" class="bg-[#F4D710] py-4 w-full text-lg font-bold rounded-2xl mt-10 btn_register cursor-pointer">
-                    <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
                     Зарегистрироваться
                 </button>
                 <p class="mt-5 text-base">Уже есть аккаунт?<a href="#" class="text-black font-bold ml-1 text_dec hover:text-[#F9D914]]">Войти</a></p>
