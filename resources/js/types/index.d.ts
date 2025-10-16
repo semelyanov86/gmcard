2c1ce3cda50750acd133219ba09652344e52e399
Kirillmlk <kostakov.kiirill@yandex.by>
2025-10-16 12:04:01 +0300

    fix: fix errord

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -36,3 +36,8 @@ export interface User {
 }
 
 export type BreadcrumbItemType = BreadcrumbItem;
+
+export type { CategoryModel } from './CategoryModel';
+export type { CityModel } from './CityModel';
+export type { ContactModel } from './ContactModel';
+export type { PromoTypeModel } from './PromoTypeModel';
--- resources/js/types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { CategoryModel } from './CategoryModel';
-export type { CityModel } from './CityModel';
-export type { ContactModel } from './ContactModel';
-export type { PromoTypeModel } from './PromoTypeModel';
