becbca2806720df1823f50e683e34fa2d02bc052
Kirillmlk <kostakov.kiirill@yandex.by>
2025-10-16 14:55:59 +0300

    refactor: replace external freepik links with placeholders

--- resources/js/components/Promo/PhotoHelpModal.vue
+++ resources/js/components/Promo/PhotoHelpModal.vue
@@ -48,32 +48,28 @@ defineEmits<{
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D1%82%D0%B8"
+                                        href="#"
                                         >Запчасти</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%AE%D1%80%D0%B8%D0%B4%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C"
+                                        href="#"
                                         >Юридическая помощь</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%9C%D0%B5%D0%B1%D0%B5%D0%BB%D1%8C"
+                                        href="#"
                                         >Мебель</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%A8%D1%83%D0%B3%D0%B0%D1%80%D0%B8%D0%BD%D0%B3"
+                                        href="#"
                                         >Шугаринг</a
                                     >
                                 </li>
@@ -82,24 +78,21 @@ defineEmits<{
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%9E%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0"
+                                        href="#"
                                         >Одежда</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=SPA%20%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D0%B4%D1%83%D1%80%D1%8B"
+                                        href="#"
                                         >SPA процедуры</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D1%81%D0%B0%D1%83%D0%BD%D1%8B"
+                                        href="#"
                                         >Сауны</a
                                     >
                                 </li>
@@ -108,24 +101,21 @@ defineEmits<{
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%A0%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%20%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80%D0%BE%D0%B2"
+                                        href="#"
                                         >Ремонт компьютеров</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%A1%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F"
+                                        href="#"
                                         >Стоматология</a
                                     >
                                 </li>
                                 <li>
                                     <a
                                         class="border-b border-dashed border-[#0066cb] text-[#0066cb] hover:border-solid"
-                                        target="_blank"
-                                        href="https://ru.freepik.com/search?format=search&query=%D0%9C%D0%B0%D0%BD%D0%B8%D0%BA%D1%8E%D1%80"
+                                        href="#"
                                         >Маникюр</a
                                     >
                                 </li>
