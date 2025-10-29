b8e1ec1da6da84c684f4504874de92008534be58
Kirillmlk <kostakov.kiirill@yandex.by>
2025-10-29 17:07:48 +0300

    feat: add bonus_balance field for users

--- database/migrations/2025_10_29_133347_add_bonus_balance_to_users_table.php
+++ database/migrations/2025_10_29_133347_add_bonus_balance_to_users_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table): void {
-            $table->integer('bonus_balance')->default(0);
+            $table->bigInteger('bonus_balance')->default(0)->nullable();
         });
     }
 
