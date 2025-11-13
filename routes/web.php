7f97d6c5ddbd2aeb2cdaff321ee9998d76777aaf
Kirillmlk <kostakov.kiirill@yandex.by>
2025-11-13 11:30:18 +0300

    feat: add Profile page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\Landing\BusinessLandingController;
 use App\Http\Controllers\Landing\UserLandingController;
 use App\Http\Controllers\Popup\FormSubmitController;
+use App\Http\Controllers\Profile\ProfileController;
 use App\Http\Controllers\Promo\CreatePromoController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,6 +16,8 @@ Route::get('/user-landing', [UserLandingController::class, 'index'])->name('user
 Route::post('/submit-form', [FormSubmitController::class, 'submit']);
 
 Route::get('dashboard', fn () => Inertia::render('Dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 
 Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('/promos/create', [CreatePromoController::class, 'index'])->name('promos.create');
