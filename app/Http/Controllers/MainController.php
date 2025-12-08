dc6755a81dd9907232d3ede4ec07fd09dff41f4a
Kirillmlk <kostakov.kiirill@yandex.by>
2025-12-08 13:09:32 +0300

    feat: add main page and contorller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,17 +6,24 @@ namespace App\Http\Controllers;
 
 use App\Actions\Category\GetCategoriesAction;
 use App\Actions\Menu\GetMenuItemsAction;
+use App\Actions\Promo\GetApprovedPromosForHomeAction;
 use App\Enums\MenuType;
+use App\Settings\GeneralSettings;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class MainController extends Controller
 {
-    public function index(): Response
+    public function index(GeneralSettings $settings): Response
     {
         return Inertia::render('MainPage', [
             'categories' => GetCategoriesAction::run(),
             'navbarMenu' => GetMenuItemsAction::run(MenuType::NAVBAR),
+            'contact' => [
+                'email' => $settings->email,
+                'phone' => $settings->phone,
+            ],
+            'promos' => GetApprovedPromosForHomeAction::run(),
         ]);
     }
 }
