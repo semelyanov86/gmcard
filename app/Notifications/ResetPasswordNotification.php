3e9e8199bb23e5cc7353da10b51a7c9bbe25608d
Kirillmlk <kostakov.kiirill@yandex.by>
2025-12-29 17:05:25 +0300

    feat: add test html

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword as BaseResetPasswordNotification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends BaseResetPasswordNotification
+{
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        $expire = config('auth.passwords.users.expire', 60);
+
+        return (new MailMessage)
+            ->subject('Восстановление пароля')
+            ->view('emails.reset-password', [
+                'url' => $url,
+                'expire' => $expire,
+            ]);
+    }
+}
+
