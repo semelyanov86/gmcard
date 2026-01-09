a7587e5af3c61ac71e53d160d60b69eacb3a4a0a
Kirillmlk <kostakov.kiirill@yandex.by>
2026-01-09 13:13:04 +0300

    refactor: refacrot css to TailWind

--- resources/js/pages/help/Help.vue
+++ resources/js/pages/help/Help.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Footer from '@/components/Footer.vue';
 import type { ContactModel } from '@/types';
+import { Link } from '@inertiajs/vue3';
 
 const { contact } = defineProps<{
     contact: ContactModel;
@@ -11,110 +12,110 @@ const { contact } = defineProps<{
     <section class="flex h-full w-full items-center justify-center px-4 py-10 md:px-0 md:text-center">
         <div class="flex flex-col items-center justify-center md:w-full">
             <div class="flex flex-col items-center justify-center">
-                <a href="/index.html">
+                <Link href="/index.html">
                     <img src="/assets/png/logo.png" alt="Логотип" class="h-14 w-14" />
-                </a>
+                </Link>
                 <h1 class="mt-3 text-4xl font-bold md:text-3xl">Один аккаунт. <span class="text-[#3399ff]">Весь мир GM!</span></h1>
                 <p class="mt-3 text-lg text-[#333333]">Один аккаунт для всех сервисов GM</p>
             </div>
-            <div class="4xl:w-full mt-8 flex w-[1100px] flex-col items-center justify-center">
+            <div class="help_content 4xl:w-full mt-8 flex flex-col items-center justify-center">
                 <h4 class="text-lg font-bold uppercase">GM справка</h4>
                 <ul class="help_list mt-10 grid grid-cols-4 gap-4">
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О GM1LP
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
-                            href="/help/user-agreement"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                    <li class="box-border h-32">
+                        <Link
+                            :href="route('help.user-agreement')"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             Пользовательское соглашение
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             Правила сообщества
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             Возможности
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе скидок
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе конкурсов
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе заданий
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе кредитования, P2P
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             Черный список
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О бартере
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе поиска вещей
-                        </a>
+                        </Link>
                     </li>
-                    <li class="box-border h-[128px]">
-                        <a
+                    <li class="box-border h-32">
+                        <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-[6px] border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
                         >
                             О сервисе для краудфандинга
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
@@ -124,17 +125,12 @@ const { contact } = defineProps<{
 </template>
 
 <style scoped>
-.help_list li {
-    min-width: 0 !important;
+.help_content {
+    width: 1100px;
 }
 
-.help_list a {
+.help_list li {
     min-width: 0 !important;
-    word-wrap: break-word !important;
-    overflow-wrap: break-word !important;
-    word-break: break-word !important;
-    white-space: normal !important;
-    line-height: 1.2 !important;
 }
 
 @media (max-width: 768px) {
