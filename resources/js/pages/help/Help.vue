314bbfe3de4d988edbbb2461dc778b09eb335905
Kirillmlk <kostakov.kiirill@yandex.by>
2026-01-09 13:29:03 +0300

    refactor: refactor collors style

--- resources/js/pages/help/Help.vue
+++ resources/js/pages/help/Help.vue
@@ -15,8 +15,8 @@ const { contact } = defineProps<{
                 <Link href="/index.html">
                     <img src="/assets/png/logo.png" alt="Логотип" class="h-14 w-14" />
                 </Link>
-                <h1 class="mt-3 text-4xl font-bold md:text-3xl">Один аккаунт. <span class="text-[#3399ff]">Весь мир GM!</span></h1>
-                <p class="mt-3 text-lg text-[#333333]">Один аккаунт для всех сервисов GM</p>
+                <h1 class="mt-3 text-4xl font-bold md:text-3xl">Один аккаунт. <span class="text-blue-500">Весь мир GM!</span></h1>
+                <p class="mt-3 text-lg text-black">Один аккаунт для всех сервисов GM</p>
             </div>
             <div class="help_content 4xl:w-full mt-8 flex flex-col items-center justify-center">
                 <h4 class="text-lg font-bold uppercase">GM справка</h4>
@@ -24,7 +24,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О GM1LP
                         </Link>
@@ -32,7 +32,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             :href="route('help.user-agreement')"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             Пользовательское соглашение
                         </Link>
@@ -40,7 +40,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             Правила сообщества
                         </Link>
@@ -48,7 +48,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             Возможности
                         </Link>
@@ -56,7 +56,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе скидок
                         </Link>
@@ -64,7 +64,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе конкурсов
                         </Link>
@@ -72,7 +72,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе заданий
                         </Link>
@@ -80,7 +80,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе кредитования, P2P
                         </Link>
@@ -88,7 +88,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             Черный список
                         </Link>
@@ -96,7 +96,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О бартере
                         </Link>
@@ -104,7 +104,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[21px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе поиска вещей
                         </Link>
@@ -112,7 +112,7 @@ const { contact } = defineProps<{
                     <li class="box-border h-32">
                         <Link
                             href="/rules.html"
-                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent bg-[#EBF0F5] px-6 text-center text-[20px] font-bold text-[#093A9C] hover:border-[#FAE115] hover:bg-white hover:px-4"
+                            class="flex h-full w-full cursor-pointer items-center justify-center border-4 border-transparent help_card_bg px-6 text-center text-xl font-bold help_card_text help_card_link hover:bg-white hover:px-4"
                         >
                             О сервисе для краудфандинга
                         </Link>
@@ -131,6 +131,18 @@ const { contact } = defineProps<{
 
 .help_list li {
     min-width: 0 !important;
+}
+
+.help_card_bg {
+    background-color: #ebf0f5;
+}
+
+.help_card_text {
+    color: #093a9c;
+}
+
+.help_card_link:hover {
+    border-color: #fae115 !important;
 }
 
 @media (max-width: 768px) {
