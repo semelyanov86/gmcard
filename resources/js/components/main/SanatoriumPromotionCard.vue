448d47933c288e63fc09f5a324459150b20db9ce
Kirillmlk <kostakov.kiirill@yandex.by>
2026-03-23 15:02:52 +0300

    fix: task fix, check

--- resources/js/components/main/SanatoriumPromotionCard.vue
+++ resources/js/components/main/SanatoriumPromotionCard.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import PromoTypeIcon from '@/components/main/PromoTypeIcon.vue';
 import type { ProfilePromo } from '@/types/promo/ProfilePromo';
-import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const props = defineProps<{
     promo: ProfilePromo;
@@ -18,22 +18,22 @@ const imageSrc = computed(() => {
 
 <template>
     <Link :href="route('promo.show', props.promo.id)" class="block">
-    <div class="main_card sanatorium-card relative mx-1 mt-11 rounded-xl">
-        <img :src="imageSrc" class="sanatorium-image w-full rounded-t-xl object-cover" alt="Товар" />
-        <div class="absolute -top-6 left-4 z-40">
-            <PromoTypeIcon :icon="props.promo.promoTypeIcon ?? null" />
-        </div>
-        <div class="down_block sanatorium-content flex flex-col justify-between rounded-b-3xl bg-white text-[#000000]">
-            <h3 class="line-clamp-2 px-6 py-4">
-                {{ props.promo.name ?? '' }}
-            </h3>
-            <div class="h-px w-full bg-black opacity-10"></div>
-            <div class="flex items-center justify-between px-6 py-4">
-                <span class="sanatorium-type font-bold">{{ props.promo.type ?? '' }}</span>
-                <PromoTypeIcon :icon="props.promo.promoTypeIcon ?? null" sizeClass="sanatorium-icon-small" />
+        <div class="main_card sanatorium-card relative mx-1 mt-11 rounded-xl">
+            <img :src="imageSrc" class="sanatorium-image w-full rounded-t-xl object-cover" alt="Товар" />
+            <div class="absolute -top-6 left-4 z-40">
+                <PromoTypeIcon :icon="props.promo.promoTypeIcon ?? null" />
+            </div>
+            <div class="down_block sanatorium-content flex flex-col justify-between rounded-b-3xl bg-white text-[#000000]">
+                <h3 class="line-clamp-2 px-6 py-4">
+                    {{ props.promo.name ?? '' }}
+                </h3>
+                <div class="h-px w-full bg-black opacity-10"></div>
+                <div class="flex items-center justify-between px-6 py-4">
+                    <span class="sanatorium-type font-bold">{{ props.promo.type ?? '' }}</span>
+                    <PromoTypeIcon :icon="props.promo.promoTypeIcon ?? null" sizeClass="sanatorium-icon-small" />
+                </div>
             </div>
         </div>
-    </div>
     </Link>
 </template>
 
