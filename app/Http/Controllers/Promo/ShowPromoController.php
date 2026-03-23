de36cee7e8f8c5a7ebf563908f557ff65f635015
Kirillmlk <kostakov.kiirill@yandex.by>
2026-03-23 14:54:32 +0300

    feature: add description for products

--- app/Http/Controllers/Promo/ShowPromoController.php
+++ app/Http/Controllers/Promo/ShowPromoController.php
@@ -7,15 +7,19 @@ namespace App\Http\Controllers\Promo;
 use App\Actions\Category\GetCategoriesAction;
 use App\Actions\Menu\GetMenuItemsAction;
 use App\Enums\MenuType;
+use App\Enums\Promo\PromoModerationStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Promo;
 use App\Settings\GeneralSettings;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class ShowPromoController extends Controller
 {
-    public function index(GeneralSettings $settings): Response
+    public function index(Promo $promo, GeneralSettings $settings): Response
     {
+        abort_if($promo->moderation_status !== PromoModerationStatus::APPROVED, 404);
+
         return Inertia::render('Promo/ShowPromo', [
             'contact' => [
                 'email' => $settings->email,
@@ -23,6 +27,11 @@ class ShowPromoController extends Controller
             ],
             'navbarMenu' => GetMenuItemsAction::run(MenuType::NAVBAR),
             'categories' => GetCategoriesAction::run(),
+            'promo' => [
+                'img' => $promo->img,
+                'description' => $promo->description,
+                'title' => $promo->name,
+            ],
         ]);
     }
 }
