0ec3b713a0f4fe68247f2c756ada26d552e4ef1a
Kirillmlk <kostakov.kiirill@yandex.by>
2026-03-24 16:15:50 +0300

    feature: add a new component that controls the free delivery icon

---
+++ resources/js/components/promoShow/PromoDeliveryBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+    label: string | null;
+}>();
+</script>
+
+<template>
+    <div class="absolute -top-6 right-10 z-10">
+        <img class="specImgSale promo-image-badge-delivery object-contain" src="/images/png/sale/delivary.png" alt="Скидка на товар" />
+        <p id="sale_for_promo" class="inline-block rounded-lg border border-gray-200 bg-white px-2 py-1 font-bold text-black shadow-sm"></p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
