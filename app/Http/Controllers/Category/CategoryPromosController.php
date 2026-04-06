69f714cf18839b6e1e181a34acdd39e7b61bf104
Kirillmlk <kostakov.kiirill@yandex.by>
2026-04-06 15:52:24 +0300

    refactor: refactor categoryPromos for category

--- app/Http/Controllers/Category/CategoryPromosController.php
+++ app/Http/Controllers/Category/CategoryPromosController.php
@@ -6,25 +6,30 @@ namespace App\Http\Controllers\Category;
 
 use App\Actions\Category\GetCategoriesAction;
 use App\Actions\Menu\GetMenuItemsAction;
-use App\Actions\Promo\GetApprovedPromosForHomeAction;
+use App\Actions\Promo\GetApprovedPromosByCategoryAction;
 use App\Enums\MenuType;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Settings\GeneralSettings;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class CategoryController extends Controller
+class CategoryPromosController extends Controller
 {
-    public function index(GeneralSettings $settings): Response
+    public function index(Category $category, GeneralSettings $settings): Response
     {
         return Inertia::render('Category/CategoryPromosPage', [
-            'categories' => GetCategoriesAction::run(),
-            'navbarMenu' => GetMenuItemsAction::run(MenuType::NAVBAR),
             'contact' => [
                 'email' => $settings->email,
                 'phone' => $settings->phone,
             ],
-            'promos' => GetApprovedPromosForHomeAction::run(),
+            'navbarMenu' => GetMenuItemsAction::run(MenuType::NAVBAR),
+            'categories' => GetCategoriesAction::run(),
+            'category' => [
+                'id' => $category->id,
+                'name' => $category->name,
+            ],
+            'promos' => GetApprovedPromosByCategoryAction::run($category),
         ]);
     }
 }
