2015261a44f6714f942d712bcafe7b9ef4b00b3e
Kirillmlk <kostakov.kiirill@yandex.by>
2026-04-08 15:15:23 +0300

    featire: add migration and ts

---
+++ database/migrations/2026_04_08_101609_create_discount_filter_options_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_filter_options', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('min_percent')->unique();
+            $table->unsignedSmallInteger('sort_order')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_filter_options');
+    }
+};
