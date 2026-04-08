74bf635d56405d51c876a4495e748ba69602edbb
Kirillmlk <kostakov.kiirill@yandex.by>
2026-04-08 15:14:59 +0300

    feature: add model for filter

---
+++ app/Models/DiscountFilterOption.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+final class DiscountFilterOption extends Model
+{
+    /** @use HasFactory<\Database\Factories\DiscountFilterOptionFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'min_percent',
+        'sort_order',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'min_percent' => 'integer',
+            'sort_order' => 'integer',
+        ];
+    }
+}
