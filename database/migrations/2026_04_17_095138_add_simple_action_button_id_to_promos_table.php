f5af5682ffa80bb2027e3050c6c16335aff5e7b1
Kirillmlk <kostakov.kiirill@yandex.by>
2026-04-17 14:34:01 +0300

    feauter: add migrations for buttons

---
+++ database/migrations/2026_04_17_095138_add_simple_action_button_id_to_promos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('promos', function (Blueprint $table) {
+            $table->foreignId('simple_action_button_id')
+                ->nullable()
+                ->after('promo_type_id')
+                ->constrained('promo_action_buttons')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('promos', function (Blueprint $table) {
+            $table->dropForeign(['simple_action_button_id']);
+            $table->dropColumn('simple_action_button_id');
+        });
+    }
+};
